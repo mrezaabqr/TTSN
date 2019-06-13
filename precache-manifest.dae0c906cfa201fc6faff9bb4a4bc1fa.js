cce159d20e2a6978c896ded736055bd250f3641b
Mohammad Reza <mrezabaqery77@gmail.com>
2019-06-13 20:10:32 +0430

    Updates

---
+++ precache-manifest.dae0c906cfa201fc6faff9bb4a4bc1fa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d8465a0d796581e0804cdbee78ac86",
+    "url": "/TTSN/index.html"
+  },
+  {
+    "revision": "baa74eb0841deea67a26",
+    "url": "/TTSN/static/css/2.7ff19518.chunk.css"
+  },
+  {
+    "revision": "7e559dbb87b81c2f5fc2",
+    "url": "/TTSN/static/css/main.0bef9318.chunk.css"
+  },
+  {
+    "revision": "baa74eb0841deea67a26",
+    "url": "/TTSN/static/js/2.40fb87ec.chunk.js"
+  },
+  {
+    "revision": "7e559dbb87b81c2f5fc2",
+    "url": "/TTSN/static/js/main.43526d3f.chunk.js"
+  },
+  {
+    "revision": "ba3fb18596f9b55d2861",
+    "url": "/TTSN/static/js/runtime~main.456e501e.js"
+  },
+  {
+    "revision": "83f4d7b9364c575ebf87f4aae4d75910",
+    "url": "/TTSN/static/media/IRANSans-Medium-web.83f4d7b9.ttf"
+  },
+  {
+    "revision": "9c764575a6851a0804dc8f5f84a2f973",
+    "url": "/TTSN/static/media/IRANSans-Medium-web.9c764575.woff"
+  },
+  {
+    "revision": "ecfc1b524b80c1754e81c7a0fd3dbf18",
+    "url": "/TTSN/static/media/IRANSans-Medium-web.ecfc1b52.eot"
+  },
+  {
+    "revision": "f20b26bfdb23ad3ded39721f03029360",
+    "url": "/TTSN/static/media/IRANSans-Medium-web.f20b26bf.woff2"
+  },
+  {
+    "revision": "57f54d47c9ff96aa9ca2475c337058d1",
+    "url": "/TTSN/static/media/IRANSans-web.57f54d47.woff"
+  },
+  {
+    "revision": "6d5e44581dd5df503a15ac8d12910655",
+    "url": "/TTSN/static/media/IRANSans-web.6d5e4458.eot"
+  },
+  {
+    "revision": "99633954a4fccdf5db8f3c701f60a67a",
+    "url": "/TTSN/static/media/IRANSans-web.99633954.ttf"
+  },
+  {
+    "revision": "ee602c4df445f46fca399ab2e7bde384",
+    "url": "/TTSN/static/media/IRANSans-web.ee602c4d.woff2"
+  }
+]);
